e3d72e9d24ece38a47af85eb230ac046056bc517
Joonhong Park <qkrwns394@naver.com>
2020-12-17 16:04:56 +0900

    대시보드 1차

--- static/js/demo/chart-bar-demo.js
+++ static/js/demo/chart-bar-demo.js
@@ -32,13 +32,13 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["January", "February", "March"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
       hoverBackgroundColor: "#2e59d9",
       borderColor: "#4e73df",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [{% for i in bar_data %}'{{ i }}',{% endfor %}],
     }],
   },
   options: {
@@ -61,7 +61,7 @@ var myBarChart = new Chart(ctx, {
           drawBorder: false
         },
         ticks: {
-          maxTicksLimit: 6
+          maxTicksLimit: 3
         },
         maxBarThickness: 25,
       }],
